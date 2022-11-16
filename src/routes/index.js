2ce3a55d70b4441f36786f3d4bc5179ef061c90d
ariggi-epam <alberto_riggi@epam.com>
2022-11-16 10:37:10 -0600

    added api route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 const orderController = require("../controllers/AlbumController");
 
 // Root/Index Routes
-router.get("/", function (req, res, next) {
+router.get("/api/", function (req, res, next) {
   res.json({
     message: "Call the /albums route to retrieve a list of albums",
   });
 });
 
 // get albums
-router.get("/albums", orderController.index);
+router.get("/api/albums", orderController.index);
 
 module.exports = router;
