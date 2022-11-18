d1774c653ab8435298176b1040ebe2feddcd3ff9
ariggi-epam <alberto_riggi@epam.com>
2022-11-18 17:25:29 -0600

    headerslogging

--- src/controllers/RolesController.js
+++ src/controllers/RolesController.js
@@ -17,7 +17,7 @@ exports.getroles = async function (req, res) {
     const accessToken=req.headers.get('x-ms-auth-token');
     console.log("access token "+accessToken);
 
-    const accessToken2=req.headers.['x-ms-auth-token'];
+    const accessToken2=req.headers['x-ms-auth-token'];
     console.log("access token 2 "+accessToken2);
     console.log(context);
     
