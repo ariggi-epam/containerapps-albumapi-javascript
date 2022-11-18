38c36b0074ccc9e97ffa3b70aa875ef68ad38c1d
ariggi-epam <alberto_riggi@epam.com>
2022-11-18 11:41:03 -0600

    get roles

---
+++ src/controllers/RolesController.js
@@ -0,0 +1,44 @@
+const fetch = require('node-fetch').default;
+
+// add role names to this object to map them to group ids in your AAD tenant
+const roleGroupMappings = {
+    //EPAM Mesón SBS
+    'admin': 'bf4e2697-e9b0-46be-8204-53cb898e4137',
+    //EPAM CSR - Meson SBS
+    'reader': 'cf83f1bd-a85c-4f7b-8574-7e09a79a084d',
+    //fake group
+    'editor': 'aaaaaaaa-a85c-4f7b-8574-7e09a79a084d'
+};
+exports.getroles = async function (req, res) {
+    const user = req.body || {};
+    const roles = [];
+    
+    for (const [role, groupId] of Object.entries(roleGroupMappings)) {
+        if (await isUserInGroup(groupId, user.accessToken)) {
+            roles.push(role);
+        }
+    }
+
+    res.json({
+        roles
+    });
+}
+
+async function isUserInGroup(groupId, bearerToken) {
+    const url = new URL('https://graph.microsoft.com/v1.0/me/memberOf');
+    url.searchParams.append('$filter', `id eq '${groupId}'`);
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`
+        },
+    });
+
+    if (response.status !== 200) {
+        return false;
+    }
+
+    const graphResponse = await response.json();
+    const matchingGroups = graphResponse.value.filter(group => group.id === groupId);
+    return matchingGroups.length > 0;
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const orderController = require("../controllers/AlbumController");
+const rolesController = require("../controllers/RolesController");
 
 // Root/Index Routes
 router.get("/api/", function (req, res, next) {
@@ -11,6 +12,7 @@ router.get("/api/", function (req, res, next) {
 
 // get albums
 router.get("/api/albums", orderController.index);
+router.get("/api/getroles",rolesController.getroles)
 router.get("/api/testdeploy", function (req, res, next) {
   res.json({
     message: "deploy done",
